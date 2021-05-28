3e4123814d48a4ae3c66a046cf8b7a44da99779e
wojowkik <wojciech.bondyra@gmail.com>
2021-05-28 23:24:34 +0200

    added BatteryComponentGraphic response to battery level

--- app/src/main/java/com/pl/wrb/BatteryComponentGraphic.java
+++ app/src/main/java/com/pl/wrb/BatteryComponentGraphic.java
@@ -2,6 +2,7 @@ package com.pl.wrb;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
@@ -11,6 +12,8 @@ import androidx.annotation.Nullable;
 public class BatteryComponentGraphic extends View
 {
     Paint p;
+    float batteryLevel = 30;
+    int batteryColor = Color.GREEN, paddingBattery = 10;
 
     public BatteryComponentGraphic(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -20,6 +23,11 @@ public class BatteryComponentGraphic extends View
     protected void onDraw(Canvas canvas)
     {
         super.onDraw(canvas);
-        canvas.drawRect(0,0,getWidth(),getHeight(), p);
+        paddingBattery = getWidth()/30;
+        canvas.drawRect(0,getHeight()/15,getWidth(),getHeight(), p);//background/border
+        canvas.drawRect((getWidth()/5)*2,0,(getWidth()/5)*3,getHeight()/15, p);//plus
+        p.setColor(batteryColor);
+        canvas.drawRect(paddingBattery,getHeight()-(getHeight()-getHeight()/15-paddingBattery)*batteryLevel/100,getWidth()-paddingBattery,getHeight()-paddingBattery, p);//main
+        p.setColor(Color.BLACK);
     }
 }
